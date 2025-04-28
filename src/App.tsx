2c68a2aecc2f56cb9c4e6f030d2501d5919427f8
Your Name <you@example.com>
2025-04-28 19:17:32 +0300

    Add link

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <button onClick={onClick}>assign</button>
       <button onClick={onClickWindowOpen}>window open</button>
+      <a href="https://localhost" target="_blank" />
     </>
   );
 }
